177b6cd8787dd3bab7ffb8ecb4a1600096e2a3e6
aambrioso <aambrioso1@gmail.com>
2021-03-18 16:52:40 -0400

    Add path code to fix image problem

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, jsonify, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from datetime import datetime, timedelta
 import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(
+    os.path.dirname(__file__), "..", "..")))
 
 
 app = Flask(__name__)
