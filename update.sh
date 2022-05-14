4d3e8b160febee6e3c2153d7ef7b23ffdb2022de
aambrioso <aambrioso1@gmail.com>
2022-05-13 20:49:43 -0400

    add update script and edit projects.html

---
+++ update.sh
@@ -0,0 +1,10 @@
+#Information on creating and executing shell scripts
+#https://www.javatpoint.com/steps-to-write-and-execute-a-shell-script
+
+#This script is used to updated the alex_site_new respository
+
+git pull
+systemctl stop alex_site_new
+systemctl start alex_site_new
+systemctl enable alex_site_new
+service nginx restart
